74f244daf241151b07705c020bd184503a6c838f
luizts <luiz.silveira@me.com.br>
2019-11-19 20:46:13 -0300

    Adding Thingiverse

--- scrapping.py
+++ scrapping.py
@@ -1,7 +1,9 @@
 from ScrapCults3D import *
 from ScrapPinshape import *
 from ScrapYeggi import *
+from ScrapThingiverse import *
 
-ScrapYeggi().start_scrapping()
+# ScrapYeggi().start_scrapping()
+ScrapThingiverse().start_scrapping()
 ScrapCults3D().start_scrapping()
 ScrapPinshape().start_scrapping()
