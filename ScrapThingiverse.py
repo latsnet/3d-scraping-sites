c2b303a42c3c5944580d50f7e2e9abd704c25cdd
luizts <latsnet@gmail.com>
2020-01-29 13:26:19 -0300

    Starting scrapping Thingverse content

--- ScrapThingiverse.py
+++ ScrapThingiverse.py
@@ -1,5 +1,6 @@
 from ScrapingEngine import ScrapingEngine
 from models import Model3D
+from bs4 import BeautifulSoup
 
 class ScrapThingiverse(ScrapingEngine):
 
@@ -29,12 +30,23 @@ class ScrapThingiverse(ScrapingEngine):
 
     def load_pages(self):
         page = 1
-        while (page < 37627):
+        number_of_models = 0
+        while (page < 10):
             url = self.get_url_page(page)
             print("Loading %s" % (url))
             html = super(ScrapThingiverse, self).get_page_content(url)
             if (html is not ""):
+                soup = BeautifulSoup(html, 'html.parser')
+
                 print("Page %s" % (page))
-            
+                cards = soup.findAll("div", {"class": "thing-card"})
+                for card in cards:
+                    print(" -> " + card.get("title"))
+                    info = card.find("div", {"class": "item-info"})
+                    url = info.find("a")
+                    number_of_models += 1
+
             page+=1
+
+        print("Number of models: %s" % (number_of_models))
 
--- scrapping.py
+++ scrapping.py
@@ -7,9 +7,9 @@ from ScrapCgtrader import *
 # ScrapYeggi().start_scrapping()
 try:
     ScrapThingiverse().start_scrapping()
-    ScrapCults3D().start_scrapping()
-    ScrapPinshape().start_scrapping()
-    ScrapCgtrader().start_scrapping()
+    #ScrapCults3D().start_scrapping()
+    #ScrapPinshape().start_scrapping()
+    #ScrapCgtrader().start_scrapping()
 
 except KeyboardInterrupt as e:
     print("Process canceled.")
